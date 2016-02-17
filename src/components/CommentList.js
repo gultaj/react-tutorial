09fc3e9aae0879764eeb73504d1ac995ede270f4
gultaj <yaschick.mail@gmail.com>
2016-02-17 08:40:02 +0200

    a

---
+++ src/components/CommentList.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Comment from './Comment';
+
+export default class CommentList extends Component {
+	render() {
+		return (
+			<div className="CommentList">
+				{this.props.data.map((comment) => {
+ 					return (<Comment author={comment.author.nickname} key={comment.id}>{comment.text}</Comment>);
+				})}
+			</div>
+		);
+	}
+}
